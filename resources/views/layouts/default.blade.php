94e8886f16b302ae55d9f41cd026fbad71cd270e
zhuxuanlv <130890727@qq.com>
2017-09-05 05:01:12 +0000

    Finish static pages

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,26 +1,9 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	    <title>@yield('title','Sample APP') - Laravel 入门教程</title>
-		<link rel="stylesheet" href="/css/app.css">
-	</head>
-	<body>
-        <header class="navbar navbar-fixed-top navbar-inverse">
-		    <div class="container">
-			    <div class="col-md-offset-1 col-md-10">
-				    <a href="/" id="logo">Sample App</a>
-					<nav>
-                        <ul class="nav navbar-nav navbar-right">
-						    <li><a href="/help">帮助</a></li>
-							<li><a href="#"></a></li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</header>
-
-		<div class="container">
-		    @yield('content')
-		</div>
-	</body>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
 </html>
