d6715a47ea64a62a150d1fe60fee33291750a1f9
zhuxuanlv <130890727@qq.com>
2017-09-05 07:23:56 +0000

    Finish user signup

--- public/js/app.js
+++ public/js/app.js
@@ -41982,7 +41982,7 @@ if (false) {
 /* 40 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n  }\n  ^\n      Invalid CSS after \"  }\": expected \"}\", was \"<<<<<<< HEAD\"\n      in /home/vagrant/Code/sample/resources/assets/sass/app.scss (line 109, column 4)\n    at runLoaders (/home/vagrant/Code/sample/node_modules/webpack/lib/NormalModule.js:194:19)\n    at /home/vagrant/Code/sample/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/vagrant/Code/sample/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/vagrant/Code/sample/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/vagrant/Code/sample/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/home/vagrant/Code/sample/node_modules/async/dist/async.js:2244:31)\n    at Object.callback (/home/vagrant/Code/sample/node_modules/async/dist/async.js:906:16)\n    at options.error (/home/vagrant/Code/sample/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
